a6bbc24af72f0a2eb53a897cc8ea7f9f6693b7d0
suci <sclyn49@gmail.com>
2024-02-26 19:03:55 -0800

    first

--- core/BaseModel.php
+++ core/BaseModel.php
@@ -6,7 +6,7 @@ class BaseModel
 
 	public function __construct()
 	{
-		$this->mysqli = new mysqli('localhost', 'root', '', 'ukk_perpus_itsma');
+		$this->mysqli = new mysqli('localhost', 'root', '', 'ukk_perpus_akbar');
 	}
 
 	public function getByUsername($username)
